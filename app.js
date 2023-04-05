0bccef7d15019fdb23fe682871fe2b1b9251e207
Kwstas A1MFREAK <kwstaspetrou123321@gmail.com>
2023-04-05 16:47:19 +0300

    fixed connection to db errors

--- app.js
+++ app.js
@@ -8,9 +8,18 @@ require('dotenv').config()
 app.set ("view engine", "ejs"); 
 app.use(bodyParser.urlencoded({extended: true})); 
 app.use(express.static("public")); 
-mongoose.set('strictQuery', true);
+mongoose.set('strictQuery', false);
  
-mongoose.connect("process.env.MONGO_URI" , {useNewUrlParser: true});
+
+const connectDB = async()=> {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
  
 const itemsSchema = {
   name: String
@@ -145,6 +154,8 @@ if (port == null || port == "") {
   port = 3000;
 }
 
-app.listen(port, function (){
-  console.log("Server has started successfully");
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  })
 });
